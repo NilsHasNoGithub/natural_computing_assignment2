3fcef6a9d0fccb1cffbff7b68baca73a924efde1
Nils Golembiewski <nilsgolembiewski@gmail.com>
2022-02-23 16:41:03 +0100

    finished ex3

--- ex3.py
+++ ex3.py
@@ -3,12 +3,10 @@ import itertools
 from typing import Callable, Tuple, Union
 import click
 import numpy as np
-from tqdm import tqdm
 
 from lib import Arr, SwarmOptimizer
 import pandas as pd
 from joblib import Parallel, delayed
-from multiprocessing import cpu_count
 import matplotlib.pyplot as plt
 import os
 from sklearn.preprocessing import LabelEncoder
@@ -43,9 +41,18 @@ def quantization_error(centroids, closest_centroids, data):
     return sum_
 
 
-def test_k_means(data, n_classes) -> Tuple[Arr, float]:
-    centroids, closest_centroids = k_means(data, n_classes, max_iter=100)
-    return centroids, quantization_error(centroids, closest_centroids, data)
+def test_k_means(data: Arr, n_classes: int, n_inits: int=10, max_iter:int=100) -> Tuple[Arr, float]:
+    all_centroids = []
+    errors = []
+
+    for _ in range(n_inits):
+        centroids, closest_centroids = k_means(data, n_classes, max_iter=max_iter)
+        all_centroids.append(centroids)
+        errors.append(quantization_error(centroids, closest_centroids, data))
+
+    best_idx = np.argmin(errors)
+
+    return all_centroids[best_idx], errors[best_idx]
 
 
 def calc_closest_centroids(data, centroids):
@@ -140,29 +147,30 @@ def plot_iris_clusters(iris_data: Arr, labels: Arr, title: str) -> "Figure":
     fig.tight_layout()
     return fig  
 
-    
+def print_err_result(typ: str, n_rounds: int, err: float):
+    print(f"Quatization error for {typ}, averaged over {n_rounds} runs: {err:.4f}")
 
 @click.command()
 @click.option(
     "--data-file", "-d", type=click.Path(exists=True), default="data/iris.data"
 )
 @click.option("--result-dir", "-r", type=click.Path(), default="results")
-def main(data_file: str, result_dir: str):
+@click.option("--n-rounds", "-n", type=click.INT, default=30)
+def main(data_file: str, result_dir: str, n_rounds: int):
     os.makedirs(result_dir, exist_ok=True)
 
     n_clusters = 3
-    n_rounds = 1
 
     iris_df = pd.read_csv(data_file, header=None)
     iris_data = iris_df.iloc[:, :-1].values
     iris_labels = iris_df.iloc[:, -1].to_list()
 
-    k_means_results = [test_k_means(iris_data, n_clusters) for _ in range(n_rounds)]
+    k_means_results = Parallel(n_jobs=-1)(delayed(test_k_means)(iris_data, n_clusters) for _ in range(n_rounds))
     k_means_error = np.mean([s[1] for s in k_means_results])
 
     k_means_result_iris = k_means_results[0][0]
 
-    print(f"Quantization error k_means, averaged over {n_rounds} runs: {k_means_error}")
+    print_err_result("k-means", n_rounds, k_means_error)
 
     n_particles = 10
     max_iter = 100
@@ -172,15 +180,15 @@ def main(data_file: str, result_dir: str):
     a2 = 1.49
 
     # quantitization error over n_rounds runs:
-    results = Parallel(n_jobs=cpu_count())(
+    results = Parallel(n_jobs=-1)(
         delayed(test_poc)(iris_data, n_clusters, n_particles, max_iter, omega, a1, a2)
-        for _ in tqdm(range(n_rounds))
+        for _ in range(n_rounds)
     )
     scores = [s[1] for s in results]
 
     poc_results_iris = results[0][0]
 
-    print(f"Quantization error POC, averaged over {n_rounds} runs: {np.mean(scores)}")
+    print_err_result("POC", n_rounds, np.mean(scores))
 
     # same for artificila data
 
@@ -190,28 +198,24 @@ def main(data_file: str, result_dir: str):
 
     artificial_data, labels = artificial_data_1()
 
-    k_means_results = [
-        test_k_means(artificial_data, n_clusters) for _ in range(n_rounds)
-    ]
+    k_means_results = Parallel(n_jobs=-1)(delayed(test_k_means)(artificial_data, n_clusters) for _ in range(n_rounds))
     k_means_error = np.mean([s[1] for s in k_means_results])
 
     k_means_centroids_artificial = k_means_results[0][0]
 
-    print(f"Quantization error k_means averaged over {n_rounds} runs: {k_means_error}")
+    print_err_result("k-means", n_rounds, k_means_error)
 
-    results = Parallel(n_jobs=cpu_count())(
+    results = Parallel(n_jobs=-1)(
         delayed(test_poc)(
             artificial_data, n_clusters, n_particles, max_iter, omega, a1, a2
         )
-        for _ in tqdm(range(n_rounds))
+        for _ in range(n_rounds)
     )
     scores = [s[1] for s in results]
 
     poc_centroids_artificial = results[0][0]
 
-    print(
-        f"Quantization error POC for artificial data, averaged over {n_rounds} runs: {np.mean(scores)}"
-    )
+    print_err_result("POC", n_rounds, np.mean(scores))
 
     # plot results for artificial data
     fig, subplots = plt.subplots(1, 3, figsize=(18, 6))
